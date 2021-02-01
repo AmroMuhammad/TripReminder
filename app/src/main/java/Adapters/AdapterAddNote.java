f400d0e76aeb8d05ce46c8234f2efd7d8d81e7bd
donia <donia.ashraf100@gmail.com>
2021-02-01 10:52:43 +0200

    finish add trip feature

---
+++ app/src/main/java/Adapters/AdapterAddNote.java
@@ -0,0 +1,81 @@
+package Adapters;
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.iti41g1.tripreminder.R;
+
+import java.util.ArrayList;
+
+import Models.NoteModel;
+
+public class AdapterAddNote extends RecyclerView.Adapter<AdapterAddNote.viewHolder> {
+    @NonNull
+
+    ArrayList<NoteModel> notesList;
+    Context context;
+    public AdapterAddNote(ArrayList<NoteModel> notesList, Context context){
+        this.context=context;
+        this.notesList=notesList;
+    }
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.note_row, parent, false);
+        EditText editText = view.findViewById(R.id.editTxtNote);
+        editText.setText("");
+        return new viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterAddNote.viewHolder holder, int position) {
+        if (notesList.get(position).getNote().length() > 0)
+            holder.note.setText(notesList.get(position).getNote());
+    }
+
+    @Override
+    public int getItemCount() {
+        return notesList.size();
+    }
+    public ArrayList<NoteModel> getNotesList(){
+        return notesList;
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder {
+        EditText note;
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            note=itemView.findViewById(R.id.editTxtNote);
+            note.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    notesList.set(getAdapterPosition(),new NoteModel(s.toString(),
+                            notesList.get(getAdapterPosition()).getChecked()));
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+
+                }
+            });
+
+        }
+        public EditText getEditText() {
+            return note ;
+        }
+
+    }
+}
--- app/src/main/java/com/iti41g1/tripreminder/controller/MainActivity.java
+++ app/src/main/java/com/iti41g1/tripreminder/controller/MainActivity.java
@@ -1,16 +1,30 @@
 package com.iti41g1.tripreminder.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
-
 import com.iti41g1.tripreminder.R;
+
+import Fragments.FragmentAddTrip;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    FragmentManager f;
+    Fragment fragmentB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         f=getSupportFragmentManager();
+        if(savedInstanceState==null){
+            fragmentB = new FragmentAddTrip();
+            f.beginTransaction().add(R.id.fragmentB, fragmentB, "fragment").commit();
+        }
+        else
+        {
+            fragmentB= (FragmentAddTrip) f.findFragmentByTag("fragment");
+        }
     }
 }
