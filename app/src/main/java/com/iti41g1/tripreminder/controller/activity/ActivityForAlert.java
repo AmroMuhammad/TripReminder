134708f75965a76e3c3d0241b8bd0ace0356eda0
Amro <amrmu94@gmail.com>
2021-02-11 04:02:27 +0200

    finished custom dialog v2

--- app/src/main/java/com/iti41g1/tripreminder/controller/activity/ActivityForAlert.java
+++ app/src/main/java/com/iti41g1/tripreminder/controller/activity/ActivityForAlert.java
@@ -1,5 +1,6 @@
 package com.iti41g1.tripreminder.controller.activity;
 
+import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -8,17 +9,24 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.room.Room;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -66,41 +74,9 @@ public class ActivityForAlert extends AppCompatActivity {
         manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         mp = MediaPlayer.create(ActivityForAlert.this, R.raw.alarmsound);
         mp.setLooping(true);
-        showDialog(this);
+        customDialog(this);
     }
 
-    private void showDialog(Context context){
-        mp.start();
-        new AlertDialog.Builder(context).setTitle("Alarm").setCancelable(false).setMessage("Your Trip "+tripName+" is now")
-                .setPositiveButton("Start", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Log.i(Constants.LOG_TAG,"hello from start screen");
-                initMap();
-                initBubble();
-                finishTrip();
-                finish();
-                mp.stop();
-            }
-        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Log.i(Constants.LOG_TAG,"hello from stop screen");
-                cancelTrip();
-                finish();
-                mp.stop();
-            }
-        }).setNeutralButton("Snooze", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                //sendNotification(context);
-                Log.i(Constants.LOG_TAG,"hello from neutral screen");
-                sendNotification();
-                finish();
-                mp.stop();
-            }
-        }).show();
-    }
 
     private void sendNotification() {
         Notification.Builder builder;
@@ -147,7 +123,7 @@ public class ActivityForAlert extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                database.tripDAO().updateTripStatus(firebaseUser,tripId,"canceled");
+                database.tripDAO().updateTripStatus(firebaseUser,tripId,"cancelled");
             }
         }).start();
     }
@@ -175,6 +151,62 @@ public class ActivityForAlert extends AppCompatActivity {
         Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
                 Uri.parse("package:" + getPackageName()));
         startActivityForResult(intent, 2084);
+    }
+
+    public void customDialog(Context context){
+        mp.start();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context,R.style.AlertDialogTheme);
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_alarm_dialog,(ConstraintLayout) findViewById(R.id.dialogLayoutContainer));
+        builder.setView(view);
+        ((TextView)view.findViewById(R.id.textTitle)).setText(Constants.APP_NAME);
+        ((TextView)view.findViewById(R.id.textMessage)).setText("Your Trip "+tripName+" is now");
+        ((Button)view.findViewById(R.id.btnSnooze)).setText(Constants.PER_DIALOG_SNOOZE);
+        ((Button)view.findViewById(R.id.btnCancel)).setText(Constants.PER_DIALOG_CANCEL);
+        ((Button)view.findViewById(R.id.btnStart)).setText(Constants.PER_DIALOG_START);
+        ((ImageView)view.findViewById(R.id.imgTitle)).setImageResource(R.drawable.ic_baseline_hourglass_bottom_24);
+
+        final AlertDialog alertDialog = builder.create();
+
+        view.findViewById(R.id.btnSnooze).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //sendNotification(context);
+                Log.i(Constants.LOG_TAG,"hello from neutral screen");
+                sendNotification();
+                finish();
+                mp.stop();
+                alertDialog.dismiss();
+            }
+        });
+
+        view.findViewById(R.id.btnCancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(Constants.LOG_TAG,"hello from stop screen");
+                cancelTrip();
+                finish();
+                mp.stop();
+                alertDialog.dismiss();
+            }
+        });
+
+        view.findViewById(R.id.btnStart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(Constants.LOG_TAG,"hello from start screen");
+                initMap();
+                initBubble();
+                finishTrip();
+                finish();
+                mp.stop();
+                alertDialog.dismiss();
+            }
+        });
+
+        if(alertDialog.getWindow() !=null){
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        alertDialog.show();
     }
 }
 
